7e267bf3ea7ec884b605bc0dda931fb98b8dab17
Max Kharlam <cerbincaerme@gmail.com>
2023-12-30 17:42:47 +0200

    improved early stop and added opportunity to save/load a model

--- functions/model.py
+++ functions/model.py
@@ -1,10 +1,17 @@
 import numpy as np
+import pickle
+from .layers import *
 
 class Main_Model:
     
     # connecting all the layers
-    def __init__(self, layers):
-        if len(layers) < 2:
+    def __init__(self, layers = None, file2load=None):
+        
+        if file2load is not None:
+            self.load_layers(file2load)
+            return
+        
+        elif len(layers) < 2:
             raise ValueError("Not enough layers. Should be at least 2")
         
         self.layers = [layers[0]]
@@ -29,7 +36,7 @@ class Main_Model:
         for layer in reversed(self.layers):
             gradient = layer.backward(gradient, learning_rate)
 
-    def train(self, X_train, y_train, X_val = None, y_val = None, epochs=10, learning_rate=0.1, lr_decay=0, step=50, val_step=100):
+    def train(self, X_train, y_train, X_val = None, y_val = None, epochs=10, learning_rate=0.1, lr_decay=0, step=50, val_step=100, verbose=False):
         # extracting amount of data
         data_amount = X_train.shape[0]
         # how many digits to be expected
@@ -37,10 +44,12 @@ class Main_Model:
         # saving initial learning rate for possible decaying if lr_decay is different from 0
         learning_rate_0 = learning_rate
         
-        if X_val is None:
+        if verbose and self.early_stop is None:
             self.history = {
-                'accuracy' : {},
-                'loss' : {},
+                "accuracy": {},     # accuracy on train data
+                "loss": {},     # loss on train data
+                'val_accuracy': {},     # accuracy on val data
+                'val_loss': {},     # accuracy on val data
             }
             
         for epoch in range(epochs):
@@ -82,6 +91,11 @@ class Main_Model:
                     # average accuracy and loss on the {step} window
                     avg_accuracy = np.mean(accuracies)
                     avg_loss = np.mean(losses)
+                    
+                    if X_val is not None:
+                        val_accuracy, val_loss = self.val_pred(X_val, y_val)
+                        self.history['val_accuracy'][epoch].append(val_accuracy)
+                        self.history['val_loss'][epoch].append(val_loss)
                     
                     accuracies = []
                     losses = []
@@ -125,7 +139,7 @@ class Main_Model:
         return np.array([np.argmax(self.forward(x), 0) for x in X])
     
     # early stop algorithm
-    def EarlyStop(self, monitor="accuracy", min_delta=.1, min_monitor = 0.6, patience=3, restore_best_layers=False):
+    def EarlyStop(self, monitor="loss", min_delta=.1, min_monitor = 1, patience=3, restore_best_layers=False):
         self.early_stop = {
             "monitor": monitor,     # What value to monitor to make early stop
             "min_delta": min_delta,     # min difference when we think that values don't change enough to continue execution
@@ -134,6 +148,7 @@ class Main_Model:
                                             # and early stop won't kick in until accuracy 0.8 has not reached
             "patience": patience,           # window in which we will check values for {min_delta}
             "restore_best_layers": restore_best_layers,     # Restore best weights for all layers {bool}
+            'LossOrAcc': True if 'loss' in monitor else False
         }
         
         self.history = {
@@ -141,7 +156,7 @@ class Main_Model:
             "loss": {},     # loss on train data
             'val_accuracy': {},     # accuracy on val data
             'val_loss': {},     # accuracy on val data
-            "global_max_index": (0, 0),     # global max {monitor} index
+            "global_minmax_index": (0, 0),     # global min or max {monitor} index, depends whether it's loss or accuracy
             "best_layers": []       # best weights for all the layers will be saved here
         }
     
@@ -151,24 +166,19 @@ class Main_Model:
         # transform into packages
         im_i = int(im_i/step) - 1
         
-        # saving metrics
-        self.history['accuracy'][epoch].append(accuracy)
-        self.history['loss'][epoch].append(loss)
-        
-        if X_val is not None:
-            val_accuracy, val_loss = self.val_pred(X_val, y_val)
-            self.history['val_accuracy'][epoch].append(val_accuracy)
-            self.history['val_loss'][epoch].append(val_loss)
-        
         # if we have enough data to to get data in {patience} window
         if ((epoch + 1) * (im_i + 1)) > self.early_stop['patience']:
             
-            # If current monitor metric is bigger than global max we update global max to this value
-            if self.history[self.early_stop['monitor']][epoch][-1] >= self.history[self.early_stop['monitor']][self.history['global_max_index'][0]][self.history['global_max_index'][1]]:
-                self.history['global_max_index'] = (epoch, im_i)
-            
+            # If current monitor metric is bigger/less than global max we update global max to this value
+            if self.early_stop['LossOrAcc']:
+                if self.history[self.early_stop['monitor']][epoch][-1] >= self.history[self.early_stop['monitor']][self.history['global_minmax_index'][0]][self.history['global_minmax_index'][1]]:
+                    self.history['global_minmax_index'] = (epoch, im_i)
+            else:
+                if self.history[self.early_stop['monitor']][epoch][-1] <= self.history[self.early_stop['monitor']][self.history['global_minmax_index'][0]][self.history['global_minmax_index'][1]]:
+                    self.history['global_minmax_index'] = (epoch, im_i)
+                    
             # and if we set {restore_best_layers} to True
-            if self.early_stop['restore_best_layers'] and (epoch, im_i) == self.history['global_max_index']:
+            if self.early_stop['restore_best_layers'] and (epoch, im_i) == self.history['global_minmax_index']:
                 # we save all the crucial data
                 self.history['best_layers'] = []
                 for layer in self.layers:
@@ -179,24 +189,44 @@ class Main_Model:
                     elif layer.__class__.__name__ == 'FCL':
                         self.history['best_layers'].append((layer.weights, layer.biases))
             
-            # extracting local min monitor metric in {patience} window
-            min_local_accuracy = min(self.history[self.early_stop['monitor']][epoch][-self.early_stop['patience']:])
-            # calculating difference between global max and local min
-            difference = abs(min_local_accuracy - self.history[self.early_stop['monitor']][self.history['global_max_index'][0]][self.history['global_max_index'][1]])
-            
-            # if min_local_accuracy is bigger then the set {min_monitor} and difference is less than {min_delta}
-            if min_local_accuracy >= self.early_stop['min_monitor'] and difference < self.early_stop['min_delta']:
-                # if we set {restore_best_layers} to True than we restore best weights
-                if self.early_stop['restore_best_layers']:
-                    for i, layer in enumerate(self.layers):
-                        if layer.__class__.__name__ == 'Conv':
-                            layer.filters = self.history['best_layers'][i]
-                        elif layer.__class__.__name__ == 'MaxPool':
-                            continue
-                        elif layer.__class__.__name__ == 'FCL':
-                            layer.weights, layer.biases = self.history['best_layers'][i]
-                # and now we stop the training process
-                return True
+            if self.early_stop['LossOrAcc']:
+                # extracting local max monitor metric in {patience} window
+                max_local_loss = max(self.history[self.early_stop['monitor']][epoch][-self.early_stop['patience']:])
+                # calculating difference between global min and local max
+                difference = abs(max_local_loss - self.history[self.early_stop['monitor']][self.history['global_minmax_index'][0]][self.history['global_minmax_index'][1]])
+                
+                # if max_local_loss is lesser then the set {min_monitor} and difference is less than {min_delta}
+                if max_local_loss <= self.early_stop['min_monitor'] and difference < self.early_stop['min_delta']:
+                    # if we set {restore_best_layers} to True than we restore best weights
+                    if self.early_stop['restore_best_layers']:
+                        for i, layer in enumerate(self.layers):
+                            if layer.__class__.__name__ == 'Conv':
+                                layer.filters = self.history['best_layers'][i]
+                            elif layer.__class__.__name__ == 'MaxPool':
+                                continue
+                            elif layer.__class__.__name__ == 'FCL':
+                                layer.weights, layer.biases = self.history['best_layers'][i]
+                    # and now we stop the training process
+                    return True
+            else:
+                # extracting local min monitor metric in {patience} window
+                min_local_accuracy = min(self.history[self.early_stop['monitor']][epoch][-self.early_stop['patience']:])
+                # calculating difference between global max and local min
+                difference = abs(min_local_accuracy - self.history[self.early_stop['monitor']][self.history['global_minmax_index'][0]][self.history['global_minmax_index'][1]])
+                
+                # if min_local_accuracy is bigger then the set {min_monitor} and difference is less than {min_delta}
+                if min_local_accuracy >= self.early_stop['min_monitor'] and difference < self.early_stop['min_delta']:
+                    # if we set {restore_best_layers} to True than we restore best weights
+                    if self.early_stop['restore_best_layers']:
+                        for i, layer in enumerate(self.layers):
+                            if layer.__class__.__name__ == 'Conv':
+                                layer.filters = self.history['best_layers'][i]
+                            elif layer.__class__.__name__ == 'MaxPool':
+                                continue
+                            elif layer.__class__.__name__ == 'FCL':
+                                layer.weights, layer.biases = self.history['best_layers'][i]
+                    # and now we stop the training process
+                    return True
         return False
     
     # same as described in EarlyStopping
@@ -209,3 +239,63 @@ class Main_Model:
                     continue
                 elif layer.__class__.__name__ == 'FCL':
                     layer.weights, layer.biases = self.history['best_layers'][i]
+                    
+    def save_layers(self, file_name):
+        layers = []
+        for layer in self.layers:
+            if layer.__class__.__name__ == 'Conv':
+                layers.append(dict(
+                    name = 'Conv',
+                    num_filters = layer.num_filters,
+                    size = layer.size,
+                    stride = layer.stride,
+                    activation = layer.activation.name,
+                    filters = layer.filters
+                ))
+            elif layer.__class__.__name__ == 'MaxPool':
+                layers.append(dict(
+                    name = 'MaxPool',
+                    size = layer.size,
+                    stride = layer.stride,
+                ))
+            elif layer.__class__.__name__ == 'FCL':
+                layers.append(dict(
+                    name = 'FCL',
+                    weights = layer.weights,
+                    biases = layer.biases,
+                    activation = layer.activation.name
+                ))
+            
+            with open(f"{file_name}.text", "wb") as file:
+                pickle.dump([layer for layer in layers], file)
+    
+    def load_layers(self, file_name):
+        layers = []
+        
+        with open(f"{file_name}.text", "rb") as file:
+            layers2load = [layer for layer in pickle.load(file)]
+                    
+        for layer in layers2load:
+            if layer['name'] == 'Conv':
+                tmp = Conv(
+                    num_filters = layer['num_filters'],
+                    size = layer['size'],
+                    stride = layer['stride'],
+                    activation = layer['activation'],
+                )
+                tmp.filters = layer['filters']
+                layers.append(tmp)
+            elif layer['name'] == 'MaxPool':
+                layers.append(
+                    MaxPool(
+                        size = layer['size'],
+                        stride = layer['stride'],
+                    )
+                )
+            elif layer['name'] == 'FCL':
+                tmp = FCL(activation = layer['activation'])
+                tmp.weights = layer['weights']
+                tmp.biases = layer['biases']
+                layers.append(tmp)
+        
+        self.__init__(layers)
